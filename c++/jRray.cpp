1474b4d517400215160660bae7e47cb012307dda
Victor Han <victor.han72@gmail.com>
2024-01-14 10:54:49 -0700

    Added a sort and a sub optimal hash

--- c++/jRray.cpp
+++ c++/jRray.cpp
@@ -1,4 +1,5 @@
 #include "jRray.h"
+#include <unordered_set>
 void jRray<typename T>::trimToSize()
 {
 
@@ -27,6 +28,33 @@ template<typename T> jRray<T>::subJrray(int fromIndex, int toIndex)
         temp.add(vec[i]);
     }   
     return temp;
+}
+void quicksort(std::vector<T> &vec, const int &left, const int &right)
+{
+    int i = left, j = right;
+    T tmp;
+    T pivot = vec[left];
+    while(i <= j)
+    {
+        while(vec[i] < pivot)
+            i++;
+        while(vec[j] > pivot)
+            j--;
+        if(i <= j)
+        {
+            tmp = vec[i];
+            vec[i] = vec[j];
+            vec[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+    if(left < j)
+        quicksort(vec, left, j);
+    if(i < right)
+        quicksort(vec, i, right);
+    
+
 }
 template<typename T> void jRray<T>::sort(Comparator<? super E> c)
 {
@@ -88,9 +116,16 @@ template<typename T> bool jRray<T>::removeAll(jRray c)
 
 
 
-template<typename T> int jRray<T>::hashCode()
+template<typename T> int64_t jRray<T>::hashCode()
 {
-
+    int64_t hash = vec.size();
+    for(int i = 0; i < vec.size(); i++)
+    {
+        hash ^= vec[i] >> 7;
+        hash |= vec[i] << 7;
+        hash += vec[i];
+    }
+    return hash
 }
 
 template<typename T> void jRray<T>::forEach(Consumer<? super T> action)
@@ -113,15 +148,43 @@ template<typename T> void jRray<T>::copyInto(T anArray[])
 }
 template<typename T> bool jRray<T>::containsAll(T c[])
 {
-
+    std::unordered_set<T> set;
+    for(int i = 0; i < vec.size(); i++)
+    {
+        set.insert(vec[i]);
+    }
 }
-template<typename T> bool jRray<T>::containsAll(std::vector<T> c)
+template<typename T> bool jRray<T>::containsAll(const std::vector<T> &c)
 {
-
+    std::unordered_set<T> set;
+    for(int i = 0; i < vec.size(); i++)
+    {
+        set.insert(vec[i]);
+    }
+    for(int i = 0; i < c.size(); i++)
+    {
+        if(set.find(c[i]) == set.end())
+        {
+            return false;
+        }
+    }
+    return true;
 }
 template<typename T> bool jRray<T>::containsAll(jRray c)
 {
-
+    std::unordered_set<T> set;
+    for(int i = 0; i < vec.size(); i++)
+    {
+        set.insert(vec[i]);
+    }
+    for(int i = 0; i < c.size(); i++)
+    {
+        if(set.find(c.at(i)) == set.end())
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 
